fb2d0cd1aa685376b16bbb9845a7468cb82ecb47
최정민 <bigdragon00@naver.com>
2025-02-18 18:03:41 +0900

    POST 아직 못 받아옴

--- app.js
+++ app.js
@@ -1,31 +1,59 @@
 import http from "http";
 import fs from "fs";
+import qs from "querystring";
 
 let dataList = [];
 
 const server = http.createServer((req, res) => {
   const url = req.url;
 
-  if (req.method === "GET") {
-    console.log(url);
-    if (url === "/") {
-      const main = fs.readFileSync("./index.html", "utf-8");
-      res.writeHead(200, "Content-Type", "utf-8", "text/html");
-      res.end(main);
-    } else if (url.endsWith(".js")) {
-      res.writeHead(200, { "Content-Type": "text/javascript" });
-      const script = fs.readFileSync(`./${req.url}`);
-      res.end(script);
-    } else if (url.endsWith(".css")) {
-      res.writeHead(200, { "Content-Type": "text/css" });
-      const css = fs.readFileSync(`./${req.url}`);
-      res.end(css);
-    } else if (url.endsWith(".html")) {
-      res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
-      const html = fs.readFileSync(`./${req.url}`, "utf-8");
-      res.end(html);
-    } else if (url === "/dataList") {
-      console.log("datalist 요청됨");
+  try {
+    if (req.method === "GET") {
+      console.log(url);
+      if (url === "/") {
+        const main = fs.readFileSync("./index.html", "utf-8");
+        res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+        res.end(main);
+      } else if (url === "/addDataPage") {
+        const addData = fs.readFileSync("./addDataPage.html", "utf-8");
+        res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+        res.end(addData);
+      } else if (url.endsWith(".js")) {
+        res.writeHead(200, { "Content-Type": "text/javascript" });
+        const script = fs.readFileSync(`./${url}`);
+        res.end(script);
+      } else if (url.endsWith(".css")) {
+        res.writeHead(200, { "Content-Type": "text/css" });
+        const css = fs.readFileSync(`./${url}`);
+        res.end(css);
+      } else if (url === "/dataList") {
+        console.log("datalist 요청됨");
+      }
+    }
+  } catch {
+    console.error("오류");
+  }
+
+  if (req.method === "POST") {
+    if (url === "/createdDataList") {
+      let list = "";
+
+      req.on("data", (frag) => {
+        list += frag;
+      });
+
+      req.end("end", () => {
+        if (list) {
+          const doc = qs.parse(list);
+          console.log("받은 글", doc);
+
+          dataList.push(doc);
+          console.log("새 글 추가", doc);
+        }
+        res.writeHead(302, { Location: "/" }); // 입력 후 새로고침
+        res.end();
+      });
+      return;
     }
   }
 });
