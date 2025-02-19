b66faae5b4d95d326db5fb490096a5e5337d1862
최정민 <bigdragon00@naver.com>
2025-02-19 09:47:37 +0900

    아니 뭐가 문제일까요?

--- fetch.js
+++ fetch.js
@@ -1,20 +1,17 @@
-async function getData() {}
-try {
-  const res = await fetch("/dataList");
-  const dataList = await res.json();
+async function getData() {
+  try {
+    const res = await fetch("/dataList");
+    const dataList = await res.json();
 
-  const tableBody = document.getElementById("datlist");
-  tableBody.innerHTML = ""; // 기존 데이터 초기화
+    const datalist = document.getElementById("datalist");
+    datalist.innerHTML = "";
 
-  dataList.forEach((item) => {
-    const row = document.createElement("tr");
-
-    row.innerHTML = `
-              <td>${item.write}</td>
-              `;
-
-    tableBody.appendChild(row);
-  });
-} catch (error) {
-  console.error("데이터 가져오기 실패:", error);
+    dataList.forEach((item) => {
+      const row = document.createElement("tr");
+      row.innerHTML = `<td>${item.write}</td>`;
+      datalist.appendChild(row);
+    });
+  } catch (error) {
+    console.error("데이터 가져오기 실패:", error);
+  }
 }
